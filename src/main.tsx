3339d81dac96a18ea4d2939bb71a5c6c19a18c9c
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2024-08-08 10:55:45 +0500

    Adding functionality to Todo app and displaying it over the web page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ContextProvider from './store/todos.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+    <ContextProvider>
     <App />
+    </ContextProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
